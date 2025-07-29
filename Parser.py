441d19579425d7caa4fed3416da3bce36e3db398
John Taylor <john.taylor@magna.com>
2025-07-29 16:30:18 -0400

    feat: Introduce base Parser class in dedicated file

---
+++ Parser.py
@@ -0,0 +1,9 @@
+# Code01.py
+# This file is part of the Code01 project.
+class parser:
+  pos = 0
+  input = ""
+
+  def constrparser(self, input_param) -> None:
+      self.input = input_param
+      return self
