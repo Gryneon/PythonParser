6b9c6ed76222ea15f9f8ca125066bf6028f42698
John Taylor <john.taylor@magna.com>
2025-08-02 02:39:07 -0400

    cleanup

--- Code01.py
+++
@@ -1,9 +0,0 @@
-# Code01.py
-# This file is part of the Code01 project.
-class parser:
-  pos = 0
-  input = ""
-
-  def constrparser(self, input_param) -> None:
-      self.input = input_param
-      return None
